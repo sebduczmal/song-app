7c002a540d6575bbdd12e974286407e4eaa2e780
Sebastian Duczmal <s.duczmal@gmail.com>
2017-11-13 23:51:14 +0100

    Adds placeholder for album covers

--- app/src/main/java/com/sebduczmal/songapp/data/SongModel.java
+++ app/src/main/java/com/sebduczmal/songapp/data/SongModel.java
@@ -5,12 +5,12 @@ import com.google.gson.annotations.SerializedName;
 
 public class SongModel {
 
-    @SerializedName("trackName") private String title;
-    @SerializedName("artistName") private String artist;
-    @SerializedName("releaseDate") private String year;
-    @SerializedName("artworkUrl100") private String thumbnailUrl;
+    @SerializedName(value="trackName", alternate = {"Song Clean"}) private String title;
+    @SerializedName(value="artistName", alternate = {"ARTIST CLEAN"}) private String artist;
+    @SerializedName(value="releaseDate", alternate = {"Release Year"}) private String year;
+    @SerializedName(value="collectionName", alternate = {"COMBINED"}) private String album;
     @SerializedName("primaryGenreName") private String genre;
-    @SerializedName("collectionName") private String album;
+    @SerializedName("artworkUrl100") private String thumbnailUrl;
 
     public String getTitle() {
         return title;
--- app/src/main/java/com/sebduczmal/songapp/util/DataBindingUtil.java
+++ app/src/main/java/com/sebduczmal/songapp/util/DataBindingUtil.java
@@ -2,6 +2,7 @@ package com.sebduczmal.songapp.util;
 
 
 import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -10,9 +11,17 @@ public class DataBindingUtil {
 
     @BindingAdapter("imageUrl")
     public static void loadImage(ImageView imageView, String imageUrl) {
-        if (imageUrl == null) {
-            return;
+        if (imageUrl != null) {
+            Picasso.with(imageView.getContext()).load(imageUrl).into(imageView);
         }
-        Picasso.with(imageView.getContext()).load(imageUrl).into(imageView);
+    }
+
+    @BindingAdapter(value = {"imageUrl", "imagePlaceholder"})
+    public static void loadImageWithPlaceholder(ImageView imageView, String imageUrl, Drawable
+            placeholder) {
+        if (imageView != null) {
+            Picasso.with(imageView.getContext()).load(imageUrl).placeholder(placeholder).into
+                    (imageView);
+        }
     }
 }
