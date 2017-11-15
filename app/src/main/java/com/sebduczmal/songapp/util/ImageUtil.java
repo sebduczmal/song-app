a3bc99b27fd0cceabde307ee5bdd897aa71794d6
Sebastian Duczmal <s.duczmal@gmail.com>
2017-11-15 04:04:16 +0100

    Tweaks details view

--- app/src/main/java/com/sebduczmal/songapp/util/ImageUtil.java
+++ app/src/main/java/com/sebduczmal/songapp/util/ImageUtil.java
@@ -6,28 +6,36 @@ import android.graphics.drawable.GradientDrawable;
 import android.support.v7.graphics.Palette;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ImageUtil {
 
     private static final float VIEW_CENTER = 0.5f;
 
     public static void radialGradientFromBitmap(Bitmap bitmap, View destinationView) {
         Palette.from(bitmap).generate(palette -> {
-            final Palette.Swatch vibrantSwatch = palette.getVibrantSwatch();
-            final Palette.Swatch dominantSwatch = palette.getDominantSwatch();
-            final Palette.Swatch darkMutedSwatch = palette.getDarkMutedSwatch();
-            if (dominantSwatch != null && vibrantSwatch != null && darkMutedSwatch != null) {
-                final GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable
-                        .Orientation.BL_TR, new int[]{
-                        vibrantSwatch.getRgb(),
-                        darkMutedSwatch.getRgb(),
-                        dominantSwatch.getRgb()});
-                gradientDrawable.setGradientType(GradientDrawable.RADIAL_GRADIENT);
-                final float backgroundWidth = destinationView.getWidth();
-                gradientDrawable.setGradientRadius(backgroundWidth);
-                gradientDrawable.setGradientCenter(VIEW_CENTER, VIEW_CENTER);
-                destinationView.setBackground(gradientDrawable);
-            }
+            final GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable
+                    .Orientation.BL_TR, getColors(palette));
+            gradientDrawable.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+            final float backgroundWidth = destinationView.getWidth();
+            gradientDrawable.setGradientRadius(backgroundWidth);
+            gradientDrawable.setGradientCenter(VIEW_CENTER, VIEW_CENTER);
+            destinationView.setBackground(gradientDrawable);
         });
     }
 
+    private static int[] getColors(Palette palette) {
+        final List<Palette.Swatch> swatches = new ArrayList<>();
+        swatches.add(palette.getVibrantSwatch());
+        swatches.add(palette.getDarkMutedSwatch());
+        swatches.add(palette.getDominantSwatch());
+        swatches.removeAll(Collections.singleton(null));
+        final int colors[] = new int[swatches.size()];
+        for (int i = 0; i < colors.length; i++) {
+            colors[i] = swatches.get(i).getRgb();
+        }
+        return colors;
+    }
 }
